c1622450b09705af513125cf0f517df5b6967622
Cheick-ahmed <chs.ahmed_pro02@outlook.com>
2020-02-18 12:14:28 +0100

    Admin link

--- layouts/components/globals/avatarDropdown.vue
+++ layouts/components/globals/avatarDropdown.vue
@@ -6,9 +6,9 @@
 			alt="User" 
 			class="object-cover object-top h-12 w-12 shadow-xl rounded-full  border-2"
 			>
-		<!-- 	<span class="font-montserrat font-semibold capitalize">
-				Maria Gardner
-			</span> -->
+			<span class="font-montserrat font-semibold capitalize">
+				{{ $auth.user.last_name }}
+			</span>
 		</button>
 		<div class="mt-4 absolute right-0  shadow-2xl font-rubik" v-if="show">
 			<div class="bg-gray-800 rounded-lg py-4 px-6 w-full text-xs uppercase text-white ">
@@ -17,6 +17,13 @@
 						<path d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
 					</svg>
 					Mon compte
+				</nuxt-link>
+
+				<nuxt-link :to="{ name : 'admin' }" class="flex items-center block py-3">
+					<svg class="fill-current h-4 w-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+						<path d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+					</svg>
+					Admin
 				</nuxt-link>
 
 				<a href="#" @click.prevent="logout" class="flex items-center block py-3">
